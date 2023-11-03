bdefd9b99a48bebd91493fcd76a1b64dc9b62ab0
AryanRathee1 <145105722+AryanRathee1@users.noreply.github.com>
2023-11-03 20:33:25 +0530

    Update Interface.sv

--- Interface.sv
+++ Interface.sv
@@ -5,15 +5,6 @@ module Interface(input clk);
   logic [7:0] res = 8'bz;  
   logic [7:0] flag = 8'b0;
   
-  // clocking block
-  /*clocking drv_cb @(posedge clk);
-    output A, B, mode, cmd, rst;
-  endclocking;
-
-  clocking mon_cb @(posedge clk);
-    input A, B, mode, cmd, flag, res;
-  endclocking;*/
-  
   modport DRV(output A, B, mode, cmd, rst);
   modport MON(input A, B, mode, cmd, flag, res);
 
